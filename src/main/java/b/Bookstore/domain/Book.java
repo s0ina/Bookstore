33123de61bdade75c079789089be4cfec1edab0a
Sonja <sonja@scoundrel.fi>
2023-03-08 17:06:27 +0200

    Spring Rest- tehtävät

--- src/main/java/b/Bookstore/domain/Book.java
+++ src/main/java/b/Bookstore/domain/Book.java
@@ -2,6 +2,8 @@ package b.Bookstore.domain;
 
 
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -23,6 +25,7 @@ public class Book {
 	private double price;
 	
 	 @ManyToOne
+	 @JsonIgnoreProperties ("books") 
 	    @JoinColumn(name = "categoryId")
 	    private Category category;
 
--- src/main/java/b/Bookstore/webcontroller/BookController.java
+++ src/main/java/b/Bookstore/webcontroller/BookController.java
@@ -1,22 +1,29 @@
 package b.Bookstore.webcontroller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import b.Bookstore.domain.Book;
 import b.Bookstore.domain.BookRepository;
 import b.Bookstore.domain.CategoryRepository;
 
-@Controller
+
+@RestController
 public class BookController {
-	
+
 	@Autowired
 	private CategoryRepository crepository;
 
@@ -30,6 +37,36 @@ public class BookController {
 
 	}
 
+	@RequestMapping(value="/api/books", method = RequestMethod.GET)
+	public @ResponseBody List<Book> bookListRest() {	
+		return (List<Book>) BookRepository.findAll();
+	}
+
+	@RequestMapping(value="/api/books/{id}", method = RequestMethod.GET)
+	public @ResponseBody Optional<Book> findBookRest(@PathVariable("id") Long bookId) {	
+		return BookRepository.findById(bookId);
+	}      
+
+
+	@PostMapping("/api/books/addbooks")
+	Book newBook(@RequestBody Book newBook) {
+		return BookRepository.save(newBook);
+	}
+
+	@DeleteMapping("/api/books/{id}")
+	public Iterable<Book> deleteBook(@PathVariable("id") Long bookId) {
+		BookRepository.deleteById(bookId);
+		return BookRepository.findAll();
+	}
+
+
+	@PutMapping("/api/books/{id}")
+	public Book editBook(@RequestBody Book editedBook, @PathVariable("id") Long bookId) {
+		editedBook.setId(bookId);
+		return BookRepository.save(editedBook);
+	}
+
+
 	@RequestMapping(value = "/addbook", method = RequestMethod.GET)
 	public String getNewBookForm(Model model) {
 		model.addAttribute("book", new Book()); 
@@ -40,26 +77,13 @@ public class BookController {
 	public String saveBook(@ModelAttribute Book book) {
 		BookRepository.save(book);
 		return "redirect:/booklist";
+
+
 	}
 
-	@RequestMapping(value = "/deletebook/{id}", method = RequestMethod.GET)
-	public String deleteBook(@PathVariable("id") Long bookId) {
-		BookRepository.deleteById(bookId);
-		return "redirect:../booklist";
-	}
-	
-	@RequestMapping(value = "/edit/{id}")
-	public String showModBook(@PathVariable("id") Long bookId, Model model) {
-	model.addAttribute("book", BookRepository.findById(bookId));
-	model.addAttribute("categories", crepository.findAll());
-	return "";
-	
-	
-	}
 
-	
-	}
-	
+}
+
 
 
 
